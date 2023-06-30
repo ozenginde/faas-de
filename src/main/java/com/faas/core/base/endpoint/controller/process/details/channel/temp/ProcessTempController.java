911bd8783c23bd915993a9edde0fd8e6a0baae8f
maxbackmannrw <max.backmannrw@gmail.com>
2023-06-30 16:57:29 +0300

    apiExecuteScenario fix

--- src/main/java/com/faas/core/base/endpoint/controller/process/details/channel/temp/ProcessTempController.java
+++ src/main/java/com/faas/core/base/endpoint/controller/process/details/channel/temp/ProcessTempController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping(value = AppConstant.API_VERSION + "/base/process/details/channel/temp/")
 public class ProcessTempController {
 
+
     @Autowired
     ProcessTempMiddleware processTempMiddleware;
 
