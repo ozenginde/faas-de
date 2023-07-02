9722c7dea192341d9ec448cd477314c6cc61a8b0
maxbackmannrw <max.backmannrw@gmail.com>
2023-07-02 16:25:57 +0300

    scenario and types refactored

--- src/main/java/com/faas/core/base/endpoint/controller/scenario/action/ActionTempController.java
+++ src/main/java/com/faas/core/base/endpoint/controller/scenario/action/ActionTempController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-@RequestMapping(value = AppConstant.API_VERSION + "/base/action/temp/")
+@RequestMapping(value = AppConstant.API_VERSION + "/base/scenario/temp/action/")
 public class ActionTempController {
 
 
--- src/main/java/com/faas/core/base/endpoint/controller/scenario/automation/AutomationTempController.java
+++ src/main/java/com/faas/core/base/endpoint/controller/scenario/automation/AutomationTempController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-@RequestMapping(value = AppConstant.API_VERSION + "/base/automation/")
+@RequestMapping(value = AppConstant.API_VERSION + "/base/scenario/temp/automation/")
 public class AutomationTempController {
 
 
