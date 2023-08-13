cd2f2f0837221e7ad583a4104d00442afc5c35cd
maxbackmannrw <max.backmannrw@gmail.com>
2023-08-13 09:05:10 +0300

    ScenarioExecutes -> ScenarioExecutions converted

--- src/main/java/com/faas/core/api/framework/operation/scenario/content/ApiScenarioFramework.java
+++ src/main/java/com/faas/core/api/framework/operation/scenario/content/ApiScenarioFramework.java
@@ -3,6 +3,7 @@ package com.faas.core.api.framework.operation.scenario.content;
 import com.faas.core.api.model.ws.operation.scenario.content.dto.*;
 import com.faas.core.base.model.db.process.details.scenario.ProcessScenarioDBModel;
 import com.faas.core.base.model.db.scenario.content.ScenarioDBModel;
+import com.faas.core.base.model.db.session.SessionDBModel;
 import com.faas.core.base.repo.operation.scenario.ScenarioExecutionRepository;
 import com.faas.core.base.repo.process.details.scenario.ProcessScenarioRepository;
 import com.faas.core.base.repo.scenario.content.ScenarioRepository;
@@ -53,16 +54,25 @@ public class ApiScenarioFramework {
 
         List<ApiScenarioWSDTO>scenarioWSDTOS = new ArrayList<>();
         List<ProcessScenarioDBModel> processScenarioDBModels = processScenarioRepository.findByProcessId(processId);
-
+        for (ProcessScenarioDBModel processScenarioDBModel : processScenarioDBModels) {
+            Optional<ScenarioDBModel> scenarioDBModel = scenarioRepository.findById(processScenarioDBModel.getScenarioId());
+            if (scenarioDBModel.isPresent()) {
+                scenarioWSDTOS.add(new ApiScenarioWSDTO(scenarioDBModel.get(), processScenarioDBModel));
+            }
+        }
         return scenarioWSDTOS;
     }
 
 
     public ApiScenarioWSDTO apiGetScenarioService(long agentId,long sessionId,String processId,String scenarioId) {
 
+        List<ProcessScenarioDBModel> processScenarioDBModels = processScenarioRepository.findByProcessIdAndScenarioId(processId,scenarioId);
+        Optional<ScenarioDBModel> scenarioDBModel = scenarioRepository.findById(scenarioId);
+        if (!processScenarioDBModels.isEmpty() && scenarioDBModel.isPresent()){
+            return new ApiScenarioWSDTO(scenarioDBModel.get(),processScenarioDBModels.get(0));
+        }
         return null;
     }
-
 
 
     public ApiScenarioDetailsWSDTO apiGetScenarioDetailsService(long agentId, long sessionId, String processId, String scenarioId) {
@@ -70,12 +80,7 @@ public class ApiScenarioFramework {
         Optional<ScenarioDBModel> scenarioDBModel = scenarioRepository.findById(scenarioId);
         List<ProcessScenarioDBModel> processScenarioDBModels = processScenarioRepository.findByProcessIdAndScenarioId(processId,scenarioId);
         if (scenarioDBModel.isPresent() && !processScenarioDBModels.isEmpty()){
-
-            ApiScenarioDetailsWSDTO scenarioDetailsWSDTO = new ApiScenarioDetailsWSDTO();
-            scenarioDetailsWSDTO.setScenario(scenarioDBModel.get());
-            scenarioDetailsWSDTO.setProcessScenario(processScenarioDBModels.get(0));
-
-            return scenarioDetailsWSDTO;
+            return new ApiScenarioDetailsWSDTO(scenarioDBModel.get(),processScenarioDBModels.get(0));
         }
         return null;
     }
@@ -91,8 +96,6 @@ public class ApiScenarioFramework {
 
         return null;
     }
-
-
 
 
 
